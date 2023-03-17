c474b8127f3f700918d1ddeab7b64d170c031d9e
jarvisisika <jarvis.isika@student.moringaschool.com>
2023-03-17 21:27:25 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
-// Write your solution in this file!
+
+var customerName = 'John';
+
+
+function upperCaseCustomerName() {
+  customerName = customerName.toUpperCase();
+}
+
+
+function setBestCustomer() {
+  bestCustomer = 'not bob';
+}
+
+
+function overwriteBestCustomer() {
+  bestCustomer = 'maybe bob';
+}
+
+
+const leastFavoriteCustomer = 'Dave';
+
+
+function changeLeastFavoriteCustomer() {
+  leastFavoriteCustomer = 'Tom'; 
+}
+
+// Declare a global variable called customerName and initialize it to 'bob'
+var customerName = 'bob';
+
+// Define a function called upperCaseCustomerName that modifies the global customerName variable to be in uppercase
+function upperCaseCustomerName() {
+  customerName = customerName.toUpperCase();
+}
